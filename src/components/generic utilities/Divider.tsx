30608c9fcaffff660656ca7b3da736678c47d5c3
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-01-02 18:42:22 +0100

    fixed bug in divider

--- src/components/generic utilities/Divider.tsx
+++ src/components/generic utilities/Divider.tsx
@@ -1,17 +1,18 @@
+import { ReactNode } from "react"
 import { PiBowlFoodDuotone } from "react-icons/pi"
 
 type DividerProps = {
-	symbol?: boolean
+	symbol?: ReactNode
 	className?: string
 }
 
-export const Divider: React.FC<DividerProps> = ({ symbol, className }) => {
+export const Divider: React.FC<DividerProps> = ({ symbol = <PiBowlFoodDuotone size={20} />, className }) => {
 	return (
-		<div className={"flex items-center " + (className ?? '')}>
+		<div className={"flex flex-row items-center w-full " + (className ?? '')}>
 			<hr className="flex-grow border-t border-gray-800" />
 			{symbol &&
 				<span className="px-3 text-gray-800">
-					<PiBowlFoodDuotone size={20} />
+					{symbol}
 				</span>
 			}
 			<hr className="flex-grow border-t border-gray-800" />
