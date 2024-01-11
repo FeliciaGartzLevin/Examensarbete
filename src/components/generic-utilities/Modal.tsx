0e3db11e00dfc18ffdb947b9188b5c9f0355666b
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-01-11 23:04:53 +0100

    fixed styling on generic modal

--- src/components/generic-utilities/Modal.tsx
+++ src/components/generic-utilities/Modal.tsx
@@ -14,7 +14,7 @@ export const Modal: React.FC<ModalProps> = ({ children, hide }) => {
 
 	return (
 		<div className="absolute top-0 h-full w-full lg:px-20 px-2 md:px-8 py-6 bg-gray-800 bg-opacity-75 flex justify-center">
-			<div ref={modalRef as React.LegacyRef<HTMLDivElement>} className='relative top-[10%] max-h-[90%] bg-light-background py-12 px-6 rounded-md flex flex-col items-center min-w-full sm:min-w-[60%] overflow-y-auto'>
+			<div ref={modalRef as React.LegacyRef<HTMLDivElement>} className='relative top-[10%] max-h-[90%] bg-light-background py-12 px-6 rounded-md flex flex-col items-center min-w-full sm:min-w-[60%] overflow-y-auto no-scrollbar'>
 
 				<button onClick={hide} title='close' type='button' className="absolute text-xs text-gray-500 top-5 right-5">
 					<IoClose size={23} />
