fa4a34db636121ea16285c7c4bbeaf7151265fa7
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-01-16 22:08:24 +0100

    changed size and positioning styling on modal

--- src/components/generic-utilities/Modal.tsx
+++ src/components/generic-utilities/Modal.tsx
@@ -13,8 +13,8 @@ export const Modal: React.FC<ModalProps> = ({ children, hide }) => {
 	useCloseOnClickOutside(modalRef, hide)
 
 	return (
-		<div className="absolute top-0 h-full w-full lg:px-20 px-2 md:px-8 py-6 bg-gray-800 bg-opacity-75 flex justify-center">
-			<div ref={modalRef as React.LegacyRef<HTMLDivElement>} className='relative top-[10%] max-h-[90%] bg-light-background py-12 px-6 rounded-md flex flex-col items-center min-w-full sm:min-w-[60%] overflow-y-auto no-scrollbar'>
+		<div className="fixed top-0 h-full w-full lg:px-20 px-2 md:px-8 py-6 bg-gray-800 bg-opacity-75 flex justify-center items-center z-20">
+			<div ref={modalRef as React.LegacyRef<HTMLDivElement>} className='relative max-h-[95%] bg-light-background py-12 px-6 rounded-md flex flex-col items-center sm:min-w-[60%] overflow-y-auto no-scrollbar'>
 
 				<button onClick={hide} title='close' type='button' className="absolute text-xs text-gray-500 top-5 right-5">
 					<IoClose size={23} />
